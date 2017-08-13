2bbe04cf74d42129f79b4cceed8b812f4d179809
David Gwyn <gwyn@dgfprofessionalservices.com>
2017-08-13 19:53:28 -0400

    Created the BMI Calculator Splash Form.

---
+++ BMICalculator/BMISplashForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Name: David Gwyn
+ * Student #: 821304912
+ * Date: August 13th, 2017
+ * Description: BMI Calculator Splash Form
+ * Version: 0.1 - Created the BMI Calculator Splash Form
+*/
+
+namespace BMICalculator
+{
+    public partial class BMISplashForm : Form
+    {
+        public BMISplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
