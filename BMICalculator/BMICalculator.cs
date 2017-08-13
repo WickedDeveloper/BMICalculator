ebf206cc882a2fcb63607dac267beec682173ec5
David Gwyn <gwyn@dgfprofessionalservices.com>
2017-08-13 17:19:48 -0400

    Added public instance variables and public properties for the BMI Calculator Class.

--- BMICalculator/BMICalculator.cs
+++ BMICalculator/BMICalculator.cs
@@ -12,17 +12,71 @@ using System.Windows.Forms;
  * Student #: 821304912
  * Date: August 13th, 2017
  * Description: BMI Calculator application
- * Version: 0.3 - Refactored the BMIResetButton_Click method to reset text boxes using the clear method.
+ * Version: 0.4 - Added public instance variables and public properties for the BMI Calculator Class.
 */
 
 namespace BMICalculator
 {
     public partial class BMICalculatorForm : Form
     {
+        // PRIVATE INSTANCE VARIABLES
+
+        private double _weight;
+        private double _height;
+        private double _bmiResult;
+
+        // PUBLIC PROPERTIES
+
+        public double Weight
+        {
+            get
+            {
+                return this._weight;
+            }
+
+            set
+            {
+                this._weight = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this._height;
+            }
+
+            set
+            {
+                this._height = value;
+            }
+        }
+
+        public double BmiResult
+        {
+            get
+            {
+                return this._bmiResult;
+            }
+
+            set
+            {
+                this._bmiResult = this._weight * this._height;
+            }
+        }
+
+
+        // CONSTRUCTORS
+
+        // PUBLIC METHODS
+
         public BMICalculatorForm()
         {
             InitializeComponent();
         }
+
+        // PRIVATE METHODS
 
         /// <summary>
         /// Resets all BMICalculatorForm controls back to defaults from initial form load.
